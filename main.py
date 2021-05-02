54c474313f6633c888f837b30ca9d4315047bcf4
FK <fkinolli@pinewood-school.gr>
2021-05-02 23:37:19 +0300

    Possible fixes?

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import discord
 from discord.ext import commands
 
-client = commands.Bot(command_prefix - '.')
+client = commands.Bot(command_prefix = '.')
 
 @client.event
 async def on_ready():
   print("I'm up and running!")
 
 @client.event
-async def on_member_join{member}:
+async def on_member_join(member):
   print(f"{member} has joined Sonder!")
 
-@cleint.event
-async def on_member_remove{member}:
+@client.event
+async def on_member_remove(member):
   print(f"{member} has left Sonder :(")
 
   client.run('ODM3OTY5MTM4OTE5OTMxOTI0.YI0Rkw.3UoV-KuzrCqurcEFPUtzzCaYpHU')

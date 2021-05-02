3f309dfe45910829f16a1ea23f6edf3e05e1087c
F K <imtehcookie@gmail.com>
2021-05-02 22:39:01 +0300

    Layed out basic groundwork

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-import
+import discord
+from discord.net import commands
+
+client = commands.Bot(command_prefix - '.')
+
+@client.event
+async def on_ready():
+  print("I'm up and running!")
+
+@client.event
+async def on_member_join(member):
+  print(f"{member} has joined Sonder!")
+
+@cleint.event
+async def on_member_remove{member}:
+  print(f"{member} has left Sonder :(")
+
+  client.run('ODM3OTY5MTM4OTE5OTMxOTI0.YI0Rkw.3UoV-KuzrCqurcEFPUtzzCaYpHU')
